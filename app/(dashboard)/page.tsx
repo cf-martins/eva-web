8898bd1263ffe84bae346094501feac95f8dfa7f
Martins <carlos.felipe@maisinteligencia.com.br>
2024-12-21 22:10:43 -0300

    feat: update layout structure in dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,15 +3,13 @@ import { ChartRadialText } from "@/components/chart-radial-text";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <ChartRadialText />
         <ChartRadialText />
         <ChartRadialText />
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-        <ChartLineLinear />
-      </div>
+      <ChartLineLinear />
     </div>
   );
 }
