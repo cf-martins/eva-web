2859c6c23ebb2d43f00f22b8f24d92b67e8830a0
Martins <carlos.felipe@maisinteligencia.com.br>
2024-12-27 23:55:46 -0300

    feat: add MDX support

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import { paraglide } from "@inlang/paraglide-next/plugin";
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   output: "standalone",
 };
+
+const withMDX = createMDX({});
 
 export default paraglide({
   paraglide: {
     project: "./project.inlang",
     outdir: "./paraglide",
   },
-  ...nextConfig,
+  ...withMDX(nextConfig),
 });
