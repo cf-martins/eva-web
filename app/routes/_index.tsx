9d3da4793ff4261d25b1b00b8fab4d25f284f380
Martins <carlos.felipe@maisinteligencia.com.br>
2025-05-23 17:59:52 -0300

    feat: set up file system routing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +8,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export default function IndexPage() {
   return <Welcome />;
 }
